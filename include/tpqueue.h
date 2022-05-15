f1201074bfc42729bfbad6f8b53cf26508d3a340
PashenkoDaria <92744987+PashenkoDaria@users.noreply.github.com>
2022-05-15 16:23:30 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,6 +4,21 @@
 
 template<typename T, int size>
 class TPQueue {
+ private:
+  T ar[100];
+  int f, l;
+ public:
+  TPQueue() : f(0), l(0) {}
+  void push(T znach) {
+    int i = l++;
+    while ((--i >= f) && (ar[i % size].prior < znach.prior)) {
+      ar[(i + 1) % size] = ar[i % size];
+    }
+    ar[(i + 1) % size] = znach;
+  }
+  T pop() {
+  return ar[(f++) % size];
+  }
   // реализация шаблона очереди с приоритетом на кольцевом буфере
 };
 
